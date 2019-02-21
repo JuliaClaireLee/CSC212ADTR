97d8d946846850b790181e53ebbb8f7c8b279a94
julialee64 <julialee64@131.229.41.229>
2019-02-21 16:33:49 -0500

    lab work

--- src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaMapTest.java
@@ -18,4 +18,29 @@ public class JavaMapTest {
 		assertEquals(empty.size(), 0);
 		//Assert.assert
 	}
+	@Test
+	public void testPut() {
+		MapADT<String, Integer> dict = new JavaMap<>();
+		dict.put("apple", 1);
+		assertEquals(dict.size(), 1);
+		//Assert.assert
+	}
+	@Test
+	public void testRemove() {
+		MapADT<String, Integer> map = new JavaMap<>();
+		map.put("apple", 1);
+		map.remove("apple");
+		assertEquals(map.size(),0);
+		//Assert.assert
+	}
+	@Test
+	public void testGet() {
+		MapADT<String, Integer> map = new JavaMap<>();
+		map.put("apple", 1);
+		assertEquals(map.get("apple"),1);
+		//Assert.assert
+	}
+	
+	
+	
 }
--- src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
+++ src/test/java/edu/smith/cs/csc212/adtr/JavaSetTest.java
@@ -13,5 +13,37 @@ public class JavaSetTest {
 		SetADT<String> empty = new JavaSet<>();
 		assertEquals(empty.size(), 0);
 	}
+	@Test
+	public void testInsertUnique() {
+		SetADT<String> abc = new JavaSet<>();
+		abc.insert("A");
+		abc.insert("B");
+		abc.insert("C");
+		assertEquals(abc.size(),3);
+	}
+	@Test
+	public void testInsertDuplicate() {
+		SetADT<String> aaa = new JavaSet<>();
+		aaa.insert("A");
+		aaa.insert("A");
+		aaa.insert("A");
+		assertEquals(aaa.size(),1);
+	}
+	@Test
+	public void testremoveDuplicate() {
+		SetADT<String> aa = new JavaSet<>();
+		aa.insert("A");
+		aa.insert("A");
+		aa.remove("A");
+		assertEquals(aa.size(),0);
+	}
+	@Test
+	public void testContains() {
+		SetADT<String> arj = new JavaSet<>();
+		arj.insert("A");
+		arj.insert("R");
+		arj.insert("J");
+		assertEquals(arj.contains("J"),true);
+	}
 	
 }
